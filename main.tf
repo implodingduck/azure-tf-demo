8761191b1d927724404a6243d2b21d02c9a50e0b
implodingduck <zsek320@gmail.com>
2021-06-08 14:57:26 -0500

    adding basic files

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.62.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+  }
+  backend "azurerm" {
+
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-azure-tf-demo"
+  location = "East US"
+}
